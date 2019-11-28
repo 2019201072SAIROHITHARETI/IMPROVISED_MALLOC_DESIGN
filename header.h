4b4fc053e965b451aff363d27aa7985f78014bce
haridatta10 <54828989+haridatta10@users.noreply.github.com>
2019-11-28 14:10:47 +0530

    Update header.h

--- header.h
+++ header.h
@@ -7,3 +7,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/mman.h>
+int Mem_IsValid(void *user_pointer);
+int Mem_Init(int total_region);
+void *Mem_Alloc(int size);
+int Mem_Free(void *pointer1);
+void garbage_collector();
+void *reallo(void *ptr);
+void *callo(int number,int size);
+int Mem_GetSize(void *ptr);
+void merging_right(void *pointer1);
+void merging_left(void *pointer1);
